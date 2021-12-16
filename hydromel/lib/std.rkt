fbb6c61893a9dbd55385ff049cada2e558b0af69
Guillaume Savaton <guillaume@baierouge.fr>
2021-12-16 21:43:51 +0100

    Naming convention for built-in operators as function names

--- hydromel/lib/std.rkt
+++ hydromel/lib/std.rkt
@@ -20,29 +20,29 @@
 
 (provide
   int->bool      int->bool:impl         int->bool:impl:return-type
-  &if            if:impl                if:impl:return-type
-  &case          case:impl              case:impl:return-type
-  &not                                  bitwise-not:return-type
-  &and                                  bitwise-and:return-type
-  &or                                   bitwise-ior:return-type
-  &xor                                  bitwise-xor:return-type
-  &==            eq:impl                eq:impl:return-type
-  &/=            ne:impl                ne:impl:return-type
-  &>             gt:impl                gt:impl:return-type
-  &<             lt:impl                lt:impl:return-type
-  &>=            ge:impl                ge:impl:return-type
-  &<=            le:impl                le:impl:return-type
-  &+                                    +:return-type
-  &-                                    -:return-type
-  &*                                    *:return-type
-  &/                                    quotient:return-type
-  &<<                                   arithmetic-shift:return-type
-  &>>            arithmetic-shift-right arithmetic-shift-right:return-type
-  &range         range:impl             range:impl:return-type
-  slice                                 unsigned-slice:return-type
-  concat         concat:impl            concat:impl:return-type
-  make-array                            vector:return-type
-  array-ref                             vector-ref:return-type
+  _if_           if:impl                if:impl:return-type
+  _case_         case:impl              case:impl:return-type
+  _not_                                 bitwise-not:return-type
+  _and_                                 bitwise-and:return-type
+  _or_                                  bitwise-ior:return-type
+  _xor_                                 bitwise-xor:return-type
+  _==_           eq:impl                eq:impl:return-type
+  _/=_           ne:impl                ne:impl:return-type
+  _>_            gt:impl                gt:impl:return-type
+  _<_            lt:impl                lt:impl:return-type
+  _>=_           ge:impl                ge:impl:return-type
+  _<=_           le:impl                le:impl:return-type
+  _+_                                   +:return-type
+  _-_                                   -:return-type
+  _*_                                   *:return-type
+  _/_                                   quotient:return-type
+  _<<_                                  arithmetic-shift:return-type
+  _>>_           arithmetic-shift-right arithmetic-shift-right:return-type
+  _range_        range:impl             range:impl:return-type
+  _slice_                               unsigned-slice:return-type
+  _concat_       concat:impl            concat:impl:return-type
+  _array_                               vector:return-type
+  _at_                                  vector-ref:return-type
   signed_width                          min-signed-width:return-type
   unsigned_width                        min-unsigned-width:return-type
   as_signed      as_signed:impl         as_signed:impl:return-type
@@ -62,8 +62,8 @@
   (t/boolean))
 
 ; The Hydromel if statement is expanded to a call-expr
-; to &if as if it were a function.
-(define-syntax &if (meta/make-function #'if:impl))
+; to _if_ as if it were a function.
+(define-syntax _if_ (meta/make-function #'if:impl))
 
 (define-syntax-parse-rule (if:impl (~seq cnd thn) ... els)
   (cond [(int->bool:impl cnd) thn]
@@ -74,8 +74,8 @@
   (t/union (list tt ... te)))
 
 ; The Hydromel case statement is expanded to a call-expr
-; to &case as if it were a function.
-(define-syntax &case (meta/make-function #'case:impl))
+; to _case_ as if it were a function.
+(define-syntax _case_ (meta/make-function #'case:impl))
 
 (define-syntax-parser case:impl
   [(_ expr (~seq ch thn) ...)
@@ -111,10 +111,10 @@
                 [_ (error "Cannot compute data size.")])))
 
 ; Boolean operators are all bitwise.
-(define-syntax &not (meta/make-function/cast #'bitwise-not))
-(define-syntax &and (meta/make-function      #'bitwise-and))
-(define-syntax &or  (meta/make-function      #'bitwise-ior))
-(define-syntax &xor (meta/make-function      #'bitwise-xor))
+(define-syntax _not_ (meta/make-function/cast #'bitwise-not))
+(define-syntax _and_ (meta/make-function      #'bitwise-and))
+(define-syntax _or_  (meta/make-function      #'bitwise-ior))
+(define-syntax _xor_ (meta/make-function      #'bitwise-xor))
 
 (define (bitwise:return-type ta tb)
   (define ta^ (t/normalize-type ta))
@@ -131,12 +131,12 @@
 (define bitwise-xor:return-type bitwise:return-type)
 
 ; Comparison operations return integers 0 and 1.
-(define-syntax &== (meta/make-function #'eq:impl))
-(define-syntax &/= (meta/make-function #'ne:impl))
-(define-syntax &>  (meta/make-function #'gt:impl))
-(define-syntax &<  (meta/make-function #'lt:impl))
-(define-syntax &>= (meta/make-function #'ge:impl))
-(define-syntax &<= (meta/make-function #'le:impl))
+(define-syntax _==_ (meta/make-function #'eq:impl))
+(define-syntax _/=_ (meta/make-function #'ne:impl))
+(define-syntax _>_  (meta/make-function #'gt:impl))
+(define-syntax _<_  (meta/make-function #'lt:impl))
+(define-syntax _>=_ (meta/make-function #'ge:impl))
+(define-syntax _<=_ (meta/make-function #'le:impl))
 
 (define (eq:impl a b)
   (if (= a b) 1 0))
@@ -167,10 +167,10 @@
 (define le:impl:return-type comparison:return-type)
 
 ; Use the built-in arithmetic operators.
-(define-syntax &+ (meta/make-function #'+))
-(define-syntax &- (meta/make-function #'-))
-(define-syntax &* (meta/make-function #'*))
-(define-syntax &/ (meta/make-function #'quotient))
+(define-syntax _+_ (meta/make-function #'+))
+(define-syntax _-_ (meta/make-function #'-))
+(define-syntax _*_ (meta/make-function #'*))
+(define-syntax _/_ (meta/make-function #'quotient))
 
 (define (+:return-type ta tb)
   (define tr (t/common-supertype (t/normalize-type ta) (t/normalize-type tb)))
@@ -195,8 +195,8 @@
 (define (quotient:return-type ta tb)
   ta)
 
-(define-syntax &<< (meta/make-function #'arithmetic-shift))
-(define-syntax &>> (meta/make-function #'arithmetic-shift-right))
+(define-syntax _<<_ (meta/make-function #'arithmetic-shift))
+(define-syntax _>>_ (meta/make-function #'arithmetic-shift-right))
 
 (define (arithmetic-shift:return-type ta tb)
   (define ta^ (t/normalize-type ta))
@@ -217,7 +217,7 @@
     [(list (t/abstract-integer na) (t/signed nb))        (t/resize ta^ (- na (min-signed-value nb)))]
     [_ (error "Shift operation expects integer operands.")]))
 
-(define-syntax &range (meta/make-function #'range:impl))
+(define-syntax _range_ (meta/make-function #'range:impl))
 
 ; TODO Empty ranges are no longer supported.
 ; TODO Do we need an explicit "descending" range specifier?
@@ -235,7 +235,7 @@
 ; The slicing operation defaults to the unsigned version.
 ; The signed case is handled automatically because the expander inserts
 ; a conversion to the type returned by the return-type.
-(define-syntax slice (meta/make-function/cast #'unsigned-slice))
+(define-syntax _slice_ (meta/make-function/cast #'unsigned-slice))
 
 (define (unsigned-slice:return-type ta tb tc)
   (define left (match tb
@@ -251,7 +251,7 @@
   (t/resize (t/normalize-type ta)
             (max 0 (add1 (- left right)))))
 
-(define-syntax concat (meta/make-function/cast #'concat:impl))
+(define-syntax _concat_ (meta/make-function/cast #'concat:impl))
 
 ; The binary concatenetion operation defaults to the unsigned version.
 ; The signed case is handled automatically because the expander inserts
@@ -272,12 +272,12 @@
     [(t/signed _)   (t/signed w)]
     [(t/unsigned _) (t/unsigned w)]))
 
-(define-syntax make-array (meta/make-function #'vector))
+(define-syntax _array_ (meta/make-function #'vector))
 
 (define (vector:return-type . ts)
   (t/array (length ts) (t/union ts)))
 
-(define-syntax array-ref (meta/make-function #'vector-ref))
+(define-syntax _at_ (meta/make-function #'vector-ref))
 
 (define (vector-ref:return-type ta tb)
   ; TODO check the type of tb
